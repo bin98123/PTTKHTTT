4aee61cc53f5128ab8adfa276831b840e2990a31
ngothanhtan280711 <80970455+ngothanhtan280711@users.noreply.github.com>
2021-05-31 23:44:47 +0700

    Tấn' s first commit
    bla bla

---
+++ PTTKHTTT/src/demo/Test.java
@@ -0,0 +1,12 @@
+package demo;
+public class Test{
+  	public static void main(String[] args) {
+          // ví dụ ông tạo 1 cái file mới này nha
+          // ở github desktop nó hiện file mới này lên
+          // h ông tạo branch mới của riêng ông, h tui vd branch ông là ThanhTan nha
+          // h ông chú ý phần branch
+          //h là push code lên branch mới đó luôn, master là branch chính, nên để dễ phân biệt ông lấy branch mới đó để push
+          //h đặt tên commit
+		System.out.println("Hello Tấn!!!");
+	}
+}
